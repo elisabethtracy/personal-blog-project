6f27c4974b540f8ad8eb92a8fb79a9cd97641e34
elisabethtracy <elisabethtracy@gmail.com>
2024-09-22 18:58:54 -0500

    Update blog.js

--- blog/blog.js
+++ blog/blog.js
@@ -5,7 +5,7 @@ const backButton = document.querySelector('#back');
 
 // TODO: Create a function that builds an element and appends it to the DOM
 
-function buildAndAppend {
+function buildAndAppend() {
     const article = document.createElement(article);
     const blogTitle = document.createElement(h2);
     blogTitle.textContent = title;
@@ -27,6 +27,8 @@ function buildAndAppend {
 // TODO: Create a function that handles the case where there are no blog posts to display
 
 function noBlogPosts() {
+    mainElement.innerHTML = '';
+    const blogPosts = JSON.parse(localStorage.getItem('blogPosts'))
     if (blogPosts.length === 0) {
         const noPostsMessage = document.createElement('p');
         noPostsMessage.textContent = "No Blog posts yet...";
